cd38036ad4c21e64783a53a416daae73831ade4c
Christian Zamora <christianzamoraburgos@gmail.com>
2021-03-02 00:40:48 -0600

    updated routes to make question html the root for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/ask', to: 'questions#ask' # '/ask' IS THE URL, questions is the controller file and ask the methods in the controller file
+  root to: 'questions#ask'
+  # get '/ask', to: 'questions#ask' # '/ask' IS THE URL, questions is the controller file and ask the methods in the controller file
   get '/answer', to: 'questions#answer' # '/answer' IS THE URL 
   # Remember the name of the controller file is only the firs word before the under score
 end
